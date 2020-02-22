ddb1c5e7d66d4ce269c5aca37b32889ccd257a4f
2220582126 <2220582126@qq.com>
2020-02-22 21:39:23 +0800

    vue练习

--- 2-18 2-19/mokuai.js
+++ 2-18 2-19/mokuai.js
@@ -1,6 +1,7 @@
 const food = '蝙蝠';
 const bing = '病毒';
 
+let name = '爱德华';
 export {
     food,
     bing
@@ -12,4 +13,8 @@ export const ai ='爱情公寓5'
 
 let demo=()=>{
     return 10+29;
+}
+
+export default{
+    name
 }
