888c00c58b911e076f733bf886817a5508d1cba2
ziyang <2220582111@qq.com>
2020-03-27 20:35:44 +0800

    vue练习

---
+++ vue练习/js/2.18.js
@@ -0,0 +1,8 @@
+const host ="localhost";
+const  title="前端大牛";
+export {
+	host,
+	title
+}
+export const app = 'niza';
+export const desc = 'youxi';
---
+++ vue练习/js/2.19.js
@@ -0,0 +1,6 @@
+let name="张杰";
+export default{
+    name
+}
+export let age="18";
+export let t="180";
